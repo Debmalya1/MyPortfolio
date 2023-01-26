2a05a39f4101ad1961a14e491e7a362dbca77ac1
Debmalya1 <109780618+Debmalya1@users.noreply.github.com>
2023-01-26 15:56:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,49 @@ for(var i=0;i<navmenuAnchorTags.length;i++)
     navmenuAnchorTags[i].addEventListener('click',function(event){
         event.preventDefault();
         var targetSectionID=this.textContent.trim().toLowerCase();
-        //console.log(targetSectionID);
-        var targetSection=document.getElementById(targetSectionID);
-        //console.log(targetSection);
+        console.log(targetSectionID);
+        if(targetSectionID == 'home')
+        {
+            return;
+        }
+        if(targetSectionID == 'contacts')
+        {
+            var targetSection=document.getElementById(targetSectionID);
+            //console.log(targetSection);
 
-        var interval=setInterval(function(){
-            var targetSectionCoordinates=targetSection.getBoundingClientRect();
-            if(targetSectionCoordinates.top<=120)
-            {
-                clearInterval(interval);
-                return;
-            }
-            window.scrollBy(0,120);
+            var interval=setInterval(function(){
+                var targetSectionCoordinates=targetSection.getBoundingClientRect();
+                if(targetSectionCoordinates.top<=120)
+                {
+                    clearInterval(interval);
+                    return;
+                }
+                window.scrollBy(0,50);
 
-            //console.log(targetSectionCoordinates.top);
-            //console.log("page offset :", window.pageYOffset);
-            //console.log(document.body.scrollHeight);
-        },25);
+                console.log(targetSectionCoordinates.top);
+                //console.log("page offset :", window.pageYOffset);
+                //console.log(document.body.scrollHeight);
+            },20);
+        }
+        else
+        {
+            var targetSection=document.getElementById(targetSectionID);
+            //console.log(targetSection);
+
+            var interval=setInterval(function(){
+                var targetSectionCoordinates=targetSection.getBoundingClientRect();
+                if(targetSectionCoordinates.top<=0)
+                {
+                    clearInterval(interval);
+                    return;
+                }
+                window.scrollBy(0,50);
+
+                console.log(targetSectionCoordinates.top);
+                //console.log("page offset :", window.pageYOffset);
+                //console.log(document.body.scrollHeight);
+            },20);
+        }
     });
 }
+
